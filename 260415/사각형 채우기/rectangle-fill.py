80dae5d47274e10cea5457a8bf35daddb1775549
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-15 11:34:11 +0900

    Title: 사각형 채우기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260415/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+UNUSED = -1
+
+memo = [UNUSED for _ in range(n + 1)]
+
+memo[1] = 1
+memo[2] = 2
+
+for idx in range(3, n + 1):
+    memo[idx] = memo[idx-1] + memo[idx-2]
+
+print(memo[n])
