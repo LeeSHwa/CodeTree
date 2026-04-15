bb4a681d853a1196647a433f9e757c1042f33ae6
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-15 11:04:09 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260415/계단 오르기/climbing-stairs.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+# Please write your code here.
+UNUSED = -1
+stairs = [UNUSED for _ in range(n + 1)]
+
+def dp(n):
+    if stairs[n] != UNUSED:
+        return stairs[n]
+    
+    if n < 5:
+        return 1
+    
+    stairs[n] = dp(n-2) + dp(n-3)
+
+    return stairs[n]
+
+print(dp(n))
+    
