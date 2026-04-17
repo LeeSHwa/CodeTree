8776435baaa7bf5c6c79ed0f8b2cac1268eb3bf9
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-04-17 17:05:54 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 260417/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,50 +1,28 @@
-from collections import deque
-
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 # memo : 해당 위치에서 뻗어나갈 수 있는 노드의 수
 memo = [[-1] * n for _ in range(n)]
 
-def bfs(row, col):
-    visited = [[False] * n for _ in range(n)]
 
-    q = deque()
+def dfs(row, col):
+    if memo[row][col] != -1:
+        return memo[row][col]
 
-    # row, col, 이동 횟수
-    q.append((row, col, 1))
-    visited[row][col] = True
+    memo[row][col] = 1
+    for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        nr = row + dr
+        nc = col + dc
 
-    max_value = 0
+        if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] > grid[row][col]:
+            memo[row][col] = max(memo[row][col], dfs(nr, nc) + 1)
 
-    while q:
-        r, c, cnt = q.popleft()
-        max_value = max(cnt, max_value)
+    return memo[row][col]
 
-        for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-            nr = r + dr
-            nc = c + dc
-
-
-            if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] > grid[r][c]:
-                # 만약 탐색하려는 memo[nr][nc]의 값이 이미 계산되어 있다면
-                if memo[nr][nc] != -1 :
-                    # 그 값을 가져와서 바로 적용
-                    memo[row][col] = memo[nr][nc] + cnt
-                    continue
-                
-                # 처음 가보는 길이라면
-                if not visited[nr][nc] :
-                    visited[nr][nc] = True
-                    q.append((nr, nc, cnt + 1))
-    
-    memo[row][col] = max(memo[row][col], max_value)
-        
 
 ans = 0
 for row in range(n):
     for col in range(n):
-        bfs(row, col)
-        ans = max(ans, memo[row][col])
+        ans = max(ans, dfs(row, col))
 
 print(ans)
