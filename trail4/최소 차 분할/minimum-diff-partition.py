404071214133a356d0ca86fc669e200f3628b613
LEE SEUNGHWA <83199487+LeeSHwa@users.noreply.github.com>
2026-05-05 12:27:22 +0900

    [Passed] 최소 차 분할 (50ms, 17MB)

--- trail4/최소 차 분할/minimum-diff-partition.py
+++ trail4/최소 차 분할/minimum-diff-partition.py
@@ -1,6 +1,7 @@
 n = int(input())
 nums = list(map(int, input().split()))
 S = sum(nums)
+target = S // 2
 
 # A의 값을 X라고 했을 때, B의 값은 자동적으로 S - X
 # 그렇다면 둘의 차이값은 |S - 2X|
@@ -8,18 +9,17 @@ S = sum(nums)
 # A 배열만 관리한다면, 차이값만 최소로 갱신해 나가면 정답?
 
 # A 배열 (만들 수 있는지?)
-dp = [False] * (S + 1)
+dp = [False] * (target + 1)
 
 dp[0] = True
 
-ans = float('inf')
-
 for num in nums:
 
-    for x in range(S, 0, -1):
-    
-        if x >= num and dp[x - num]:
+    for x in range(target, num - 1, -1):
+        if dp[x - num]:
             dp[x] = True
-            ans = min(ans, abs(S - 2*x))
 
-print(ans)
+for i in range(target, -1, -1):
+    if dp[i]:
+        print(S - 2 * i)
+        break
